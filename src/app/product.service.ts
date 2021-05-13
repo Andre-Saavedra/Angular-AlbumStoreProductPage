3a1331076527b654143c07491e1f6a51c28c3a83
RODRIGUES SAAVEDRA André João <andrejoao.rodriguessaavedra@altran.com>
2021-05-13 12:24:27 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
+import { HttpClient} from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -7,10 +8,11 @@ export class ProductService {
 
   private _albumUrl = '../assts/album.json';
 
-  constructor(private _http: Http, _productService: ProductService) { }
+  constructor(private _http: HttpClient, _productService: ProductService) { }
 
   getAlbum(id: number){
-   return this._http.get(this._albumUrl).map((response) => response.json());
+   return this._http.get(this._albumUrl)
+   //.map((response) => response.json());
   }
 
 }
