4a32768e0f0c913e816feece27dd52e7d152fd0e
RODRIGUES SAAVEDRA André João <andrejoao.rodriguessaavedra@altran.com>
2021-05-13 12:01:05 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
-import { } from 'rxjs/add/operator/map';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
-  constructor(private _http: Http, _productService: ProductService) { }
-
   private _albumUrl = '../assts/album.json';
 
+  constructor(private _http: Http, _productService: ProductService) { }
+
   getAlbum(id: number){
-   return this._http.get(this._albumUrl).map(reponse.json);
+   return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
 }
